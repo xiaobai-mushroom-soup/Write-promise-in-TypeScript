62782287a04c9a87b7d764d4fd9aa1270bf067aa
grz <1605537551@qq.com>
2023-03-15 01:03:11 +0800

    promise:Implement access resolveAndreject change state

--- myPromise.ts
+++ myPromise.ts
@@ -8,9 +8,21 @@ const enum PROMISE_STATUS {
   STATUS_REJECTED = "rejected",
 }
 class myPromise<T> {
+  PromiseState: string;
   constructor(f: Executor<T>) {
-    const resolve = () => {};
-    const reject = () => {};
+    this.PromiseState = PROMISE_STATUS.STATUS_PENDING;
+
+    const resolve = () => {
+      if (this.PromiseState === PROMISE_STATUS.STATUS_PENDING) {
+        this.PromiseState = PROMISE_STATUS.STATUS_FULFILLED;
+      }
+    };
+    const reject = () => {
+      if (this.PromiseState === PROMISE_STATUS.STATUS_PENDING) {
+        this.PromiseState = PROMISE_STATUS.STATUS_REJECTED;
+      }
+    };
+    f(resolve, reject);
   }
   then<U>(g: (result: T) => myPromise<U>): myPromise<U> {}
   catch<U>(g: (error: unknown) => myPromise<U>): myPromise<U> {}
