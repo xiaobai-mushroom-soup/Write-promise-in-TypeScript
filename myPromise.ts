d579f92d46b1dee1a1e88637ce1b1dd5390acc53
grz <1605537551@qq.com>
2023-03-15 01:47:56 +0800

    promise:done

--- myPromise.ts
+++ myPromise.ts
@@ -7,10 +7,23 @@ const enum PROMISE_STATUS {
   STATUS_FULFILLED = "fulfilled",
   STATUS_REJECTED = "rejected",
 }
+
+const execFunWithCatchErr = <T>(execFn:Function,value:T,resolve:Function,reject:Function)=>{
+    try{
+        const result = execFn(value);
+        resolve(result)
+    }catch(err){
+        if(err instanceof Error){
+            reject(err)
+        }
+    }
+}
 class myPromise<T> {
   PromiseState: string;
   value: T | undefined;
   reason: unknown | undefined;
+  onFullfilledFns ?:Function[]
+  onRejectedFns ?:Function[]
   constructor(f: Executor<T>) {
     this.PromiseState = PROMISE_STATUS.STATUS_PENDING;
 
@@ -18,12 +31,22 @@ class myPromise<T> {
       if (this.PromiseState === PROMISE_STATUS.STATUS_PENDING) {
         this.PromiseState = PROMISE_STATUS.STATUS_FULFILLED;
         this.value = result;
+        queueMicrotask(()=>{
+            this.onFullfilledFns!.forEach(( fn : any ) => {
+              fn(this.value);
+            });
+        })
       }
     };
     const reject = (error: unknown) => {
       if (this.PromiseState === PROMISE_STATUS.STATUS_PENDING) {
         this.PromiseState = PROMISE_STATUS.STATUS_REJECTED;
         this.reason = error;
+         queueMicrotask(() => {
+           this.onRejectedFns!.forEach(( fn : any ) => {
+             fn(this.reason);
+           });
+         });
       }
     };
     try {
@@ -34,6 +57,32 @@ class myPromise<T> {
       }
     }
   }
-  then<U>(g: (result: T) => myPromise<U>): myPromise<U> {}
-  catch<U>(g: (error: unknown) => myPromise<U>): myPromise<U> {}
+  then<U>(g?:(result: T) => myPromise<U> ,e?: (error: unknown) => myPromise<U>): myPromise<U> {
+    g = g || ( v => { throw v } )
+    e = e || ( r => { throw r } )
+ return new myPromise((resolve, reject) => {
+   // 当状态为pending的时候进行的逻辑
+   if (this.PromiseState === PROMISE_STATUS.STATUS_PENDING) {
+     // 入栈操作
+     if (g)
+       this.onFullfilledFns!.push(() => {
+         execFunWithCatchErr(g as Function, this.value, resolve, reject);
+       });
+     if (e)
+       this.onRejectedFns!.push(() => {
+         execFunWithCatchErr(e as Function, this.reason, resolve, reject);
+       });
+   }
+   if (this.PromiseState === PROMISE_STATUS.STATUS_FULFILLED && g) {
+     execFunWithCatchErr(e as Function, this.value, resolve, reject);
+   }
+   if (this.PromiseState === PROMISE_STATUS.STATUS_REJECTED && e) {
+     execFunWithCatchErr(e, this.reason, resolve, reject);
+   }
+ });  	
+   
+  }
+  catch<U>(g?: (error: unknown) => myPromise<U>): myPromise<U> {
+   return this.then(undefined, g);
+  }
 }
