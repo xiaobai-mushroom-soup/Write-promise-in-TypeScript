5ae5478ec5d585add751ad75dab5af914e5ad38e
grz <1605537551@qq.com>
2023-03-15 00:55:16 +0800

    promise:create three status and implement Resolve&Reject

--- myPromise.ts
+++ myPromise.ts
@@ -2,9 +2,16 @@ type Executor<T> = (
   resolve: (result: T) => void,
   reject: (error: unknown) => void
 ) => void;
-
+const enum PROMISE_STATUS {
+  STATUS_PENDING = "pending",
+  STATUS_FULFILLED = "fulfilled",
+  STATUS_REJECTED = "rejected",
+}
 class myPromise<T> {
-  constructor(f: Executor<T>) {}
+  constructor(f: Executor<T>) {
+    const resolve = () => {};
+    const reject = () => {};
+  }
   then<U>(g: (result: T) => myPromise<U>): myPromise<U> {}
-  catch<U>(g: (error: unknown) => myPromise<U>): myPromise<U>{};
+  catch<U>(g: (error: unknown) => myPromise<U>): myPromise<U> {}
 }
